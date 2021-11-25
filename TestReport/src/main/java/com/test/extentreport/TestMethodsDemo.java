9195ed81d595e2495cdd97b0fa4f06a9eace3474
qiran51 <liwanjing5_1@163.com>
2021-11-25 21:20:55 +0800

    修改xml文件中artifactId

--- TestReport/src/main/java/com/test/extentreport/TestMethodsDemo.java
+++ TestReport/src/main/java/com/test/extentreport/TestMethodsDemo.java
@@ -1,9 +1,11 @@
 package com.test.extentreport;
 
+import org.testng.annotations.Test;
+
 public class TestMethodsDemo {
 
-
+    @Test
     public void Test1(){
-
+        System.out.println("1");
     }
 }
